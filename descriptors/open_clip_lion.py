de641c29e7d7de8ff8653a4c58206a5ee99581f4
Rahel Arnold <rahel.arnold@unibas.ch>
2023-10-09 09:46:21 +0200

    cleaned up CLIP feature

--- descriptors/open_clip_lion.py
+++ descriptors/open_clip_lion.py
@@ -1,17 +1,14 @@
 import base64
 from io import BytesIO
-
+import json
 from apiflask import APIBlueprint
 from flask import request
-
-from core.main import device as device
-
-open_clip_lion = APIBlueprint('open_clip_lion', __name__)
-
-import json
+from core.main import device
 import open_clip
 import torch
 from PIL import Image
+
+open_clip_lion = APIBlueprint('open_clip_lion', __name__)
 
 model, _, preprocess = open_clip.create_model_and_transforms('xlm-roberta-base-ViT-B-32',
                                                              pretrained='laion5b_s13b_b90k')
@@ -22,24 +19,25 @@ tokenizer = open_clip.get_tokenizer('xlm-roberta-base-ViT-B-32')
 @open_clip_lion.get("/retrieve/clip/text/<string:text>")
 @open_clip_lion.doc(summary="CLIP endpoint for feature extraction on text")
 def clip_text(text):
-    if text is None:
-        feature = "[]"
-    else:
-        feature = json.dumps(feature_text(text).tolist())
+    feature = "[]" if text is None else json.dumps(feature_text(text).tolist())
     return feature
 
 
 @open_clip_lion.post("/retrieve/clip/image")
 @open_clip_lion.doc(
-    summary="CLIP endpoint for feature extraction on image, where the image is transmitted in the body by a data URL")
+    summary="CLIP endpoint for feature extraction on image, where the image is transmitted in the body by a data URL"
+)
 def clip_image():
-    data = request.form['data']
+    data = request.form.get('data', '')
     header, encoded = data.split("base64,", 1)
-    image = Image.open(BytesIO(base64.b64decode(encoded)))
-    if image is None:
-        feature = "[]"
-    else:
-        feature = json.dumps(feature_image(image).tolist())
+
+    try:
+        image = Image.open(BytesIO(base64.b64decode(encoded)))
+    except Exception as e:
+        print(f"Error decoding image: {e}")
+        return "[]"
+
+    feature = "[]" if image is None else json.dumps(feature_image(image).tolist())
     return feature
 
 
