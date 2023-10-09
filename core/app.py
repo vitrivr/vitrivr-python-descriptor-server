20d028c621c8653d227c9a52b19c8835927e7f12
Rahel Arnold <rahel.arnold@unibas.ch>
2023-10-09 09:40:06 +0200

    added DINOv2 feature implementation

--- core/app.py
+++ core/app.py
@@ -4,11 +4,13 @@ app = APIFlask(__name__, title='ExternalPython Descriptor Server API for vitrivr
 
 # import necessary modules
 from descriptors.open_clip_lion import open_clip_lion
+from descriptors.dino_v2 import dino_v2
 
 
 # specify here all modules, that will be needed for feature extraction server
 def register_modules():
     app.register_blueprint(open_clip_lion)
+    app.register_blueprint(dino_v2)
 
 
 def entrypoint(host, port, args):
---
+++ descriptors/dino_v2.py
@@ -0,0 +1,44 @@
+import base64
+from io import BytesIO
+
+import torch
+import torchvision.transforms as transforms
+
+from PIL import Image
+from apiflask import APIBlueprint
+from flask import request
+
+dino_v2 = APIBlueprint('dino_v2', __name__)
+
+# Load DINOv2 model
+dinov2_model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14').eval()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dino_model = dinov2_model.to(device)
+
+# Image transformation
+transform_image = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize(244),
+    transforms.CenterCrop(224),
+    transforms.Normalize([0.5], [0.5])
+])
+
+
+@dino_v2.post("/retrieve/dino")
+@dino_v2.doc(
+    summary="DINOv2 endpoint for feature extraction on image, where the image is transmitted in the body by a data URL"
+)
+def dino_image():
+    data = request.form.get('data', '')
+    header, encoded = data.split("base64,", 1)
+
+    try:
+        image = Image.open(BytesIO(base64.b64decode(encoded)))
+    except Exception as e:
+        print(f"Error decoding image: {e}")
+        return "[]"
+
+    transformed_img = transform_image(image)[:3].unsqueeze(0)
+    feature = dino_model(transformed_img).detach().numpy().flatten().tolist()
+
+    return feature
